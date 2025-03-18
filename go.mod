9543b05789cead0f452202387e7f57d06b2b8960
Aditya <aditya@fisip.net>
2025-03-18 23:12:47 +0700

    add parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Aex5/module-test
+module github.com/Aex5/module-test/v2
 
 go 1.23.1
--- sayhello.go
+++ sayhello.go
@@ -1,5 +1,5 @@
 package gomodule
 
-func SayHello() string {
-  return "Hello, World! from me"
+func SayHello(name string) string {
+  return "Hello, " + name
 }
